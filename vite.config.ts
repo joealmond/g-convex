955764c46bc4c3604ff161d41415c2774e0400de
joealmond <jozsef.mandula@gmail.com>
2026-01-19 23:56:49 +0100

    feat: Add Netlify Vite plugin for TanStack Start and update gitignore.

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite'
 import tsConfigPaths from 'vite-tsconfig-paths'
 import tailwindcss from '@tailwindcss/vite'
 import viteReact from '@vitejs/plugin-react'
+import netlifyPlugin from '@netlify/vite-plugin-tanstack-start'
 
 export default defineConfig({
   server: {
@@ -19,6 +20,6 @@ export default defineConfig({
     }),
     tanstackStart(),
     viteReact(),
+    netlifyPlugin(),
   ],
 })
-
